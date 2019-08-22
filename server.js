98000ca159046b864ec5793cf8ed685be142d742
unknown <tcutlip08@gmail.com>
2019-08-22 19:20:02 -0400

    merged index starter and added boiler for the other 2 index. also npm installed everything need for back end

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var express = require('express');
+var path = require('path');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/', function (req, res) {
-    res.send('Welcome to the Page!');
+
+var tables = [
+    {
+        name: "first table",
+        phoneNumber: 7704444444,
+        email: "bob@aol.com",
+        uniqueID: "damnit bobby"
+    }
+];
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "table.html"));
+});
+
+app.get("/waiting", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
 });
